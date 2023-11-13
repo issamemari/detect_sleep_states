44b99206a7cf1b5af9ff4f1d780d6c78f5c9434c
Issa Memari <issa.memari@gmail.com>
2023-11-13 23:42:40 +0100

    Implement loss function

--- src/model/anchors.py
+++ src/model/anchors.py
@@ -1,9 +1,7 @@
-import torch
 from typing import List
 
+import torch
 
-# Parameters
-ANCHOR_SCALES = [512, 2048, 4096, 8192, 12288, 16384] # TODO: Move to model config
 
 # Generate anchors for a 1D signal
 def generate_anchors(signal_length: int, anchor_scales: List[int]):
@@ -13,4 +11,28 @@ def generate_anchors(signal_length: int, anchor_scales: List[int]):
         for point in anchor_points:
             anchors.append((point - scale / 2, point + scale / 2))
 
-    return torch.tensor(anchors)
+    return torch.tensor(anchors) / signal_length
+
+
+def construct_ground_truth(anchors, ground_truth_boxes, num_classes):
+    spatial = 625
+    batch_size = 1
+    num_anchors = 56
+
+    gt_classes = torch.zeros((batch_size, spatial, num_anchors, num_classes))
+    gt_bboxes = torch.zeros((batch_size, spatial, num_anchors, 2))
+
+    for b in range(batch_size):
+        for _, box in enumerate(ground_truth_boxes[b]):
+            # Find the best matching anchor for each ground truth box
+            best_anchor = find_best_anchor(anchors, box)
+
+            # Calculate the offsets for the bounding box relative to the anchor
+            gt_bboxes[b, :, best_anchor, :] = encode_offsets(anchors[best_anchor], box)
+
+            # Set the class label for the matched anchor
+            gt_classes[b, :, best_anchor, box.class_id] = 1
+
+            # Mark negative and ignore anchors in gt_classes appropriately
+
+    return gt_classes, gt_bboxes
--- src/model/cnn.py
+++ src/model/cnn.py
@@ -1,20 +1,20 @@
+from typing import List
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-from typing import List
 from anchors import generate_anchors
 
 
 class OneDObjectDetectionCNN(nn.Module):
     def __init__(
-            self,
-            *,
-            signal_length: int,
-            input_channels: int,
-            num_classes: int,
-            anchor_scales: List[int],
-        ):
+        self,
+        *,
+        signal_length: int,
+        input_channels: int,
+        num_classes: int,
+        anchor_scales: List[int],
+    ):
         super(OneDObjectDetectionCNN, self).__init__()
 
         self.input_channels = input_channels
@@ -33,6 +33,7 @@ class OneDObjectDetectionCNN(nn.Module):
             nn.Conv1d(32, 64, kernel_size=33, padding=16),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.AdaptiveAvgPool1d(1),
         )
 
         # Generate anchors
@@ -41,7 +42,9 @@ class OneDObjectDetectionCNN(nn.Module):
 
         # Prediction layers
         self.score_pred = nn.Conv1d(64, self.num_anchors * num_classes, kernel_size=1)
-        self.bbox_pred = nn.Conv1d(64, self.num_anchors * 2, kernel_size=1)  # Each anchor has 2 coordinates (start, end)
+        self.bbox_pred = nn.Conv1d(
+            64, self.num_anchors * 2, kernel_size=1
+        )  # Each anchor has 2 coordinates (start, end)
 
     def forward(self, x):
         # Forward pass through the network
@@ -53,12 +56,79 @@ class OneDObjectDetectionCNN(nn.Module):
 
         # Reshape the output for easy interpretation
         scores = scores.permute(0, 2, 1).contiguous()
-        scores = scores.view(scores.size(0), scores.size(1), self.num_anchors, self.num_classes)
+        scores = scores.view(scores.size(0), self.num_anchors, self.num_classes)
 
         bboxes = bboxes.permute(0, 2, 1).contiguous()
-        bboxes = bboxes.view(bboxes.size(0), bboxes.size(1), self.num_anchors, 2)
+        bboxes = bboxes.view(bboxes.size(0), self.num_anchors, 2)
 
         return scores, bboxes
+
+
+class OneDObjectDetectionLoss(nn.Module):
+    def calculate_ious(self, anchors: torch.Tensor, box: torch.Tensor) -> torch.Tensor:
+        """
+        Calculate the IoU between a ground truth box and a set of anchors
+        """
+        # Calculate the intersection of the box and anchors
+        intersection_start = torch.max(anchors[:, 0], box[0])
+        intersection_end = torch.min(anchors[:, 1], box[1])
+        intersection = torch.clamp(intersection_end - intersection_start, min=0)
+
+        # Calculate the areas
+        anchor_areas = anchors[:, 1] - anchors[:, 0]
+        box_area = box[1] - box[0]
+
+        # Calculate the union of the box and anchors
+        union = anchor_areas + box_area - intersection
+
+        # Calculate the IoU
+        ious = intersection / union
+
+        return ious
+
+    def find_best_anchor(self, anchors: torch.Tensor, box: torch.Tensor) -> int:
+        """
+        Find the best matching anchor for a given ground truth box
+        """
+        # Calculate the IoU between the ground truth box and each anchor
+        ious = self.calculate_ious(anchors, box)
+
+        # Find the anchor with the highest IoU
+        best_anchor = torch.argmax(ious)
+
+        return best_anchor
+
+    def forward(self, scores, bboxes, gt_classes, gt_bboxes, anchors) -> float:
+        num_anchors = anchors.size(0)
+        num_classes = scores.size(2)
+
+        scores = scores.view(-1, num_anchors, num_classes)
+
+        gt_bboxes = gt_bboxes.view(-1, 2)
+        gt_classes = gt_classes.view(-1)
+
+        # get the best anchor for each ground truth box
+        best_anchors = []
+        for b in range(gt_bboxes.size(0)):
+            best_anchors.append(self.find_best_anchor(anchors, gt_bboxes[b]))
+
+        scores_ready = torch.zeros_like(scores)
+        scores_ready[range(scores.size(0)), best_anchors, gt_classes] = 1
+
+        bboxes_ready = torch.zeros_like(bboxes)
+        bboxes_ready[range(bboxes.size(0)), best_anchors, :] = gt_bboxes
+
+        # for bboxes, keep only best anchors
+        bboxes_ready = bboxes_ready[range(bboxes.size(0)), best_anchors, :]
+        bboxes = bboxes[range(bboxes.size(0)), best_anchors, :]
+
+        classification_loss = F.binary_cross_entropy_with_logits(
+            scores, scores_ready, reduction="sum"
+        )
+
+        regression_loss = F.smooth_l1_loss(bboxes, bboxes_ready, reduction="sum")
+
+        return classification_loss + regression_loss
 
 
 def main():
@@ -68,20 +138,28 @@ def main():
         signal_length=signal_length,
         input_channels=2,
         num_classes=2,
-        anchor_scales=[512, 2048, 4096, 8192, 12288, 16384]
+        anchor_scales=[512, 2048, 4096, 8192, 12288, 16384],
     )
 
-    # Example input (batch size of 1, 2 channel, signal length of 256)
-    input_signal = torch.randn(1, 2, signal_length)
+    # Example input (batch size of 1, 2 channel, signal length of signal_length)
+    input_signal = torch.randn(4, 2, signal_length)
 
     # Get model predictions
     scores, bboxes = model(input_signal)
 
-    print("Scores shape:", scores.shape)
-    print("Bounding boxes shape:", bboxes.shape)
+    # Example ground truth
+    gt_classes = torch.tensor([[1], [1], [1], [1]])
+    gt_bboxes = (
+        torch.tensor([[[0, 120]], [[5, 1024]], [[3060, 4000]], [[5, 1024]]]).float()
+        / signal_length
+    )
 
-    import code
-    code.interact(local=locals())
+    # Compute loss
+    loss_fn = OneDObjectDetectionLoss()
+
+    loss = loss_fn(scores, bboxes, gt_classes, gt_bboxes, model.anchors)
+
+    print(loss)
 
 
 if __name__ == "__main__":
